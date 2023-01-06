8fb32b34bc5bcd7af2cffb156d36264fb5fc1f67
Jacob Orshalick <jacob@solutionsfit.com>
2023-01-06 11:07:35 -0600

    adding a custom profile image and first version of a blog post

--- components/alert.tsx
+++ components/alert.tsx
@@ -1,6 +1,6 @@
 import Container from './container'
 import cn from 'classnames'
-import { EXAMPLE_PATH } from '../lib/constants'
+import { PERSONAL_BLOG_PATH } from '../lib/constants'
 
 type Props = {
   preview?: boolean
@@ -29,9 +29,9 @@ const Alert = ({ preview }: Props) => {
             </>
           ) : (
             <>
-              The source code for this blog is{' '}
+              The source code for my blog is{' '}
               <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
+                href={PERSONAL_BLOG_PATH}
                 className="underline hover:text-blue-600 duration-200 transition-colors"
               >
                 available on GitHub
