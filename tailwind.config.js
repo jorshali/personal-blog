6529635b5c935e688ea9362b13b22371cd8d36a7
Jacob Orshalick <jacob@solutionsfit.com>
2023-01-23 17:31:47 -0600

    ensure styles are applied to all components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/components/**/*.tsx', './src/pages/**/*.tsx'],
+  content: ['./src/**/*.tsx' ],
   theme: {
     extend: {
       fontSize: {
