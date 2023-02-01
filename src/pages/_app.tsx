98da4612d7bd2a2ee96e8da0ad58ffc2f1f951d0
Jacob Orshalick <jacob@solutionsfit.com>
2023-02-01 07:15:39 -0600

    add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,25 @@
 import "../styles/index.css";
 
 import { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-xxxxxxxxxx"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-N9RF3LYM9T');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
